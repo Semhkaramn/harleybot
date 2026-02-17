c7b1778aad5b1f7e3beee1b9ddc1c2f6e7d1058b
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2026-02-17 12:27:37 +0300

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,6 +1,8 @@
+# Command guard must be imported FIRST to intercept commands before other handlers
+from bot.handlers import command_guard
 from bot.handlers import basic
 from bot.handlers import filters
 from bot.handlers import tagger
 from bot.handlers import admin
 
-__all__ = ["basic", "filters", "tagger", "admin"]
+__all__ = ["command_guard", "basic", "filters", "tagger", "admin"]
