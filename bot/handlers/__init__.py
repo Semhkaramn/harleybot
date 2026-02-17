61cf77dc0f4c4511c9b083415a4071ea35836ce8
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2026-02-17 12:10:36 +0300

    Create __init__.py

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,6 @@
+from bot.handlers import basic
+from bot.handlers import filters
+from bot.handlers import tagger
+from bot.handlers import admin
+
+__all__ = ["basic", "filters", "tagger", "admin"]
