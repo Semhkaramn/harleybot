81464c76c0c380689186d571f94534949fcd1af3
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2026-02-17 13:23:33 +0300

    Update __init__.py

--- bot/database/__init__.py
+++ bot/database/__init__.py
@@ -1,4 +1,4 @@
-from bot.database.connection import init_db, close_db, get_pool
+from bot.database.connection import init_db, close_db, get_db
 from bot.database.filters import *
 from bot.database.members import *
 from bot.database.settings import *
